f451ff70b46bd184df7e7b0f8ae75e5f55d2cd68
Jorge <jorgepm914@gmail.com>
2022-11-01 00:51:51 -0600

    ND

--- PIA_APP_LOCALES/SISTEMA_NOMINA/Form_Menu.Designer.cs
+++ PIA_APP_LOCALES/SISTEMA_NOMINA/Form_Menu.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.Panael_LeftMenu = new System.Windows.Forms.Panel();
-            this.Panel_Logo = new System.Windows.Forms.Panel();
-            this.Panel_MenuOpciones = new System.Windows.Forms.Panel();
-            this.btn_Inicio = new System.Windows.Forms.Button();
-            this.btn_Timbra = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.btn_ExplorarRec = new System.Windows.Forms.Button();
             this.btn_SalirDeEmpresa = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Panel_MenuOpciones = new System.Windows.Forms.Panel();
+            this.btn_ExplorarRec = new System.Windows.Forms.Button();
+            this.btn_Timbra = new System.Windows.Forms.Button();
+            this.btn_Inicio = new System.Windows.Forms.Button();
+            this.Panel_Logo = new System.Windows.Forms.Panel();
             this.Panael_LeftMenu.SuspendLayout();
-            this.Panel_MenuOpciones.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.Panel_MenuOpciones.SuspendLayout();
             this.SuspendLayout();
             // 
             // Panael_LeftMenu
@@ -51,16 +51,42 @@
             this.Panael_LeftMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.Panael_LeftMenu.Location = new System.Drawing.Point(0, 0);
             this.Panael_LeftMenu.Name = "Panael_LeftMenu";
-            this.Panael_LeftMenu.Size = new System.Drawing.Size(200, 582);
+            this.Panael_LeftMenu.Size = new System.Drawing.Size(200, 555);
             this.Panael_LeftMenu.TabIndex = 0;
             // 
-            // Panel_Logo
+            // panel2
             // 
-            this.Panel_Logo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Panel_Logo.Location = new System.Drawing.Point(0, 0);
-            this.Panel_Logo.Name = "Panel_Logo";
-            this.Panel_Logo.Size = new System.Drawing.Size(200, 186);
-            this.Panel_Logo.TabIndex = 0;
+            this.panel2.Controls.Add(this.btn_SalirDeEmpresa);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 495);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 56);
+            this.panel2.TabIndex = 3;
+            // 
+            // btn_SalirDeEmpresa
+            // 
+            this.btn_SalirDeEmpresa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(126)))), ((int)(((byte)(228)))));
+            this.btn_SalirDeEmpresa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_SalirDeEmpresa.FlatAppearance.BorderSize = 0;
+            this.btn_SalirDeEmpresa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SalirDeEmpresa.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SalirDeEmpresa.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_SalirDeEmpresa.Location = new System.Drawing.Point(0, 0);
+            this.btn_SalirDeEmpresa.Name = "btn_SalirDeEmpresa";
+            this.btn_SalirDeEmpresa.Size = new System.Drawing.Size(200, 56);
+            this.btn_SalirDeEmpresa.TabIndex = 0;
+            this.btn_SalirDeEmpresa.Text = "Salir de la Empresa";
+            this.btn_SalirDeEmpresa.UseVisualStyleBackColor = false;
+            this.btn_SalirDeEmpresa.Click += new System.EventHandler(this.btn_SalirDeEmpresa_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 335);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 160);
+            this.panel1.TabIndex = 2;
             // 
             // Panel_MenuOpciones
             // 
@@ -68,84 +94,80 @@
             this.Panel_MenuOpciones.Controls.Add(this.btn_Timbra);
             this.Panel_MenuOpciones.Controls.Add(this.btn_Inicio);
             this.Panel_MenuOpciones.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Panel_MenuOpciones.Location = new System.Drawing.Point(0, 186);
+            this.Panel_MenuOpciones.Location = new System.Drawing.Point(0, 158);
             this.Panel_MenuOpciones.Name = "Panel_MenuOpciones";
             this.Panel_MenuOpciones.Size = new System.Drawing.Size(200, 177);
             this.Panel_MenuOpciones.TabIndex = 1;
             // 
-            // btn_Inicio
-            // 
-            this.btn_Inicio.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_Inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Inicio.Location = new System.Drawing.Point(0, 0);
-            this.btn_Inicio.Name = "btn_Inicio";
-            this.btn_Inicio.Size = new System.Drawing.Size(200, 57);
-            this.btn_Inicio.TabIndex = 0;
-            this.btn_Inicio.Text = "Inicio";
-            this.btn_Inicio.UseVisualStyleBackColor = true;
-            // 
-            // btn_Timbra
-            // 
-            this.btn_Timbra.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_Timbra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Timbra.Location = new System.Drawing.Point(0, 57);
-            this.btn_Timbra.Name = "btn_Timbra";
-            this.btn_Timbra.Size = new System.Drawing.Size(200, 63);
-            this.btn_Timbra.TabIndex = 1;
-            this.btn_Timbra.Text = "Timbra";
-            this.btn_Timbra.UseVisualStyleBackColor = true;
-            this.btn_Timbra.Click += new System.EventHandler(this.btn_Timbra_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 363);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 160);
-            this.panel1.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.btn_SalirDeEmpresa);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 523);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 56);
-            this.panel2.TabIndex = 3;
-            // 
             // btn_ExplorarRec
             // 
+            this.btn_ExplorarRec.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
             this.btn_ExplorarRec.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_ExplorarRec.FlatAppearance.BorderSize = 0;
             this.btn_ExplorarRec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ExplorarRec.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ExplorarRec.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btn_ExplorarRec.Location = new System.Drawing.Point(0, 120);
             this.btn_ExplorarRec.Name = "btn_ExplorarRec";
             this.btn_ExplorarRec.Size = new System.Drawing.Size(200, 63);
             this.btn_ExplorarRec.TabIndex = 2;
             this.btn_ExplorarRec.Text = "Explorar Recibos";
-            this.btn_ExplorarRec.UseVisualStyleBackColor = true;
+            this.btn_ExplorarRec.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_ExplorarRec.UseVisualStyleBackColor = false;
             // 
-            // btn_SalirDeEmpresa
+            // btn_Timbra
             // 
-            this.btn_SalirDeEmpresa.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_SalirDeEmpresa.Location = new System.Drawing.Point(0, 0);
-            this.btn_SalirDeEmpresa.Name = "btn_SalirDeEmpresa";
-            this.btn_SalirDeEmpresa.Size = new System.Drawing.Size(200, 56);
-            this.btn_SalirDeEmpresa.TabIndex = 0;
-            this.btn_SalirDeEmpresa.Text = "Salir de la Empresa";
-            this.btn_SalirDeEmpresa.UseVisualStyleBackColor = true;
-            this.btn_SalirDeEmpresa.Click += new System.EventHandler(this.btn_SalirDeEmpresa_Click);
+            this.btn_Timbra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            this.btn_Timbra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Timbra.FlatAppearance.BorderSize = 0;
+            this.btn_Timbra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Timbra.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Timbra.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Timbra.Location = new System.Drawing.Point(0, 57);
+            this.btn_Timbra.Name = "btn_Timbra";
+            this.btn_Timbra.Size = new System.Drawing.Size(200, 63);
+            this.btn_Timbra.TabIndex = 1;
+            this.btn_Timbra.Text = "Timbra";
+            this.btn_Timbra.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Timbra.UseVisualStyleBackColor = false;
+            this.btn_Timbra.Click += new System.EventHandler(this.btn_Timbra_Click);
+            // 
+            // btn_Inicio
+            // 
+            this.btn_Inicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            this.btn_Inicio.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Inicio.FlatAppearance.BorderSize = 0;
+            this.btn_Inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Inicio.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Inicio.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Inicio.Location = new System.Drawing.Point(0, 0);
+            this.btn_Inicio.Name = "btn_Inicio";
+            this.btn_Inicio.Size = new System.Drawing.Size(200, 57);
+            this.btn_Inicio.TabIndex = 0;
+            this.btn_Inicio.Text = "Inicio";
+            this.btn_Inicio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Inicio.UseVisualStyleBackColor = false;
+            // 
+            // Panel_Logo
+            // 
+            this.Panel_Logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(42)))), ((int)(((byte)(67)))));
+            this.Panel_Logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_Logo.Location = new System.Drawing.Point(0, 0);
+            this.Panel_Logo.Name = "Panel_Logo";
+            this.Panel_Logo.Size = new System.Drawing.Size(200, 158);
+            this.Panel_Logo.TabIndex = 0;
             // 
             // Form_Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 582);
+            this.ClientSize = new System.Drawing.Size(1103, 555);
             this.Controls.Add(this.Panael_LeftMenu);
             this.Name = "Form_Menu";
             this.Text = "Form_Menu";
             this.Panael_LeftMenu.ResumeLayout(false);
-            this.Panel_MenuOpciones.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.Panel_MenuOpciones.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
